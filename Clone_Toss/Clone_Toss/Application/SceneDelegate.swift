7468a365fd743c149f149843fc848b18dded6d96
Hansangjin98 <onetable98@naver.com>
2022-07-14 04:59:08 +0900

    [Add] 프로젝트 초기 파일들 추가

---
+++ Clone_Toss/Clone_Toss/Application/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  Clone_Toss
+//
+//  Created by 한상진 on 2022/07/14.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = HomeViewController()
+    }
+}
+
---
+++ Clone_Toss/Clone_Toss/Sources/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clone_Toss
+//
+//  Created by 한상진 on 2022/07/14.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
